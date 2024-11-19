c628fb54257088126245babc3e98591baf9dfb9e
rookie <jf1033224672@gmail.com>
2024-11-19 20:32:03 +0800

    实现双控开关+gtkwave

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -3,18 +3,32 @@
 #include <assert.h>
 #include "Vtop.h"
 #include "verilated.h"
+#include <verilated_vcd_c.h>
 
 int main(int argc, char** argv) {
     VerilatedContext* contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
     Vtop* top = new Vtop{contextp};
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *m_trace = new VerilatedVcdC;
+    top->trace(m_trace, 5);
+    m_trace->open("waveform.vcd");
+
+    vluint64_t sim_time = 0;
+
     while(true) {
         int a = rand() & 1;
         int b = rand() & 1;
         top->a = a;
         top->b = b;
         top->eval();
+        m_trace->dump(sim_time++);
         printf("a = %d, b = %d, f = %d\n", a, b, top->f);
         assert(top->f == (a ^ b));
     }
+
+    m_trace->close();
+    delete top;
+    exit(EXIT_SUCCESS);
 }
